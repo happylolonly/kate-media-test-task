056d98afa8301723fc466527db724c5c0068f2e5
Cheslav Zhuravskiy <happylolonly@gmail.com>
2020-05-16 15:00:17 +0300

    change token logic

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,20 +2,15 @@ import bodyParser from "body-parser";
 import UserModel from "../models/user.js";
 
 export async function fakeAuth(req, res, next) {
+  // token is user ID
   const token = req.headers.authorization;
 
-  if (!token && !fakeAuth[token]) {
+  if (!token) {
     res.status(403).end();
   }
 
-  const fakeRoles = {
-    1: "principal",
-    2: "teacher",
-    3: "student"
-  };
-
-  const user = await UserModel.find({ role: fakeRoles[token] });
-  req.user = user[0].toJSON();
+  const user = await UserModel.findById(token);
+  req.user = user.toJSON();
 
   console.log("Token:", token);
   console.log("User:", user);
