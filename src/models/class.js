e51257b1ab26f631f8dbecdc9b3fec07e4bb344e
Cheslav Zhuravskiy <happylolonly@gmail.com>
2020-05-18 12:20:10 +0300

    extend class model

--- src/models/class.js
+++ src/models/class.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
+  schoolId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "School"
+  },
+  teacherId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
   students: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -12,8 +20,30 @@ const schema = new mongoose.Schema({
       type: mongoose.Schema.Types.ObjectId,
       ref: "User"
     }
-  ]
+  ],
+  scheduleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Schedule"
+  }
 });
+
+schema.pre("save", next => {
+  // not sure about this
+
+  if (!this.schoolId || !this.teacherId) {
+    next("Class should belongs to School or Teacher");
+    return;
+  }
+
+  // if (this.schoolId) {
+  //   delete this.teacherId;
+  //   // allow teachers array
+  // } else if (this.teacherId) {
+  //   delete this.schoolId;
+  //   delete this.teachers;
+  // }
+});
+
 const Class = mongoose.model("class", schema);
 
 export default Class;
